3bb52f6e89d8bad35bcfec5268a6668121eb7618
Arjun Baindur <xagent@gmail.com>
2019-06-27 01:42:33 +0000

    Add skeleton python packaging code

---
+++ du_debug/du_debug.py
@@ -0,0 +1,16 @@
+import sys
+from oslo_config import cfg
+from oslo_log import log as logging
+
+CONF = cfg.CONF
+
+
+def main():
+    logging.register_options(CONF)
+    logging.set_defaults()
+    CONF(sys.argv[1:])
+
+    print "Hello world, all the packets are correct"
+
+if __name__ == '__main__':
+    main()
