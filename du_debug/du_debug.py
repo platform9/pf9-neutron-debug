b8adae7fa44bad1fda944789e01b5c98d578488e
Pranav <pranav@platform9.com>
2019-06-28 13:19:03 -0700

    RabbitMQTest

--- du_debug/du_debug.py
+++ du_debug/du_debug.py
@@ -1,23 +1,59 @@
+# Host Server for RPC messaging
+
 import sys
 from oslo_config import cfg
 from oslo_log import log as logging
 
 CONF = cfg.CONF
 
+grp = cfg.OptGroup('mygroup')
+CONF.register_group(grp)
+
+logging.register_options(CONF)
+logging.set_defaults()
+
+
+class TestEndpoint(object):
+    target = oslo.messaging.Target(namespace='test', version='2.0')
+
+    def __init__(self, server):
+        self.server = server
+
+    def hello_world(self, ctx, name):
+        return "Hello my name is %s!" % (name)
+
 
 def main():
-    logging.register_options(CONF)
-    logging.set_defaults()
-  
-    grp = cfg.OptGroup('mygroup')
-	 
 
     CONF(sys.argv[1:])
 
-    print "Hello world, all the packets are correct"
+    server = create_server(CONF)
+    server.start()
+    server.wait()
+    stop_server(server)
 
-def create_server(conf)
-	
+
+def create_server(conf):
+    """
+    Create RPC server for handling messaging
+    """
+    oslo.messaging.set_transport_defaults('myexchange')
+    transport = oslo.messaging.get_transport(conf)
+    target = oslo.messaging.Target(topic='myroutingkey', server='myserver')
+    endpoints = [TestEndpoint(None)]
+    server = oslo.messaging.get_rpc_server(transport, target, endpoints,
+                                      executor='eventlet')
+    return server
+
+
+def stop_server(rpc_server):
+    LOG.info('Stopping registry RPC server')
+    if rpc_server.listener:
+        rpc_server.stop()
+        rpc_server.wait()
+    else:
+        LOG.info('No rpc listener, nothing to stop')
+
 
 if __name__ == '__main__':
     main()
--- host_debug/host_debug.py
+++ host_debug/host_debug.py
@@ -5,13 +5,27 @@ from oslo_log import log as logging
 CONF = cfg.CONF
 
 
+
 def main():
     logging.register_options(CONF)
     logging.set_defaults()
     CONF(sys.argv[1:])
 
-    print "Hello world, all the packets are correct"
+    oslo.messaging.set_transport_defaults('myexchange')
+
+    recieve_message()
+
+
+def recieve_message():
+    transport = oslo.messaging.get_transport(CONF,
+                     url='<insert DU URL>')
+    target = oslo.messaging.Target(topic='myroutingkey', version='2.0',
+                                   namespace='test')
+    client = oslo.messaging.RPCClient(transport, target)
+    r = client.call({}, 'hello_world', name='Neutron Debugger')
+    print r
+
+
 
 if __name__ == '__main__':
     main()
-
