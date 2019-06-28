325a17f93679ddef73830cb14ddc91c68a0e117f
Pranav Narasimmaraj <pranav@platform9.com>
2019-06-28 10:51:07 -0700

    test pipeline

--- du_debug/du_debug.py
+++ du_debug/du_debug.py
@@ -8,9 +8,16 @@ CONF = cfg.CONF
 def main():
     logging.register_options(CONF)
     logging.set_defaults()
+  
+    grp = cfg.OptGroup('mygroup')
+	 
+
     CONF(sys.argv[1:])
 
     print "Hello world, all the packets are correct"
+
+def create_server(conf)
+	
 
 if __name__ == '__main__':
     main()
