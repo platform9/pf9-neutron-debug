720de3dc741e92f437b22bb70454f55e5237d833
Pranav Narasimmaraj <pranav@platform9.com>
2019-07-22 14:09:13 -0700

    Add network id to dnsmasq msg

--- host_debug/dhcp/dnsmasq_checker.py
+++ host_debug/dhcp/dnsmasq_checker.py
@@ -3,7 +3,7 @@ import os
 
 def init_dnsmasq_check(dhcp_dict, host_id):
     error_code = analyze_dnsmasq(dhcp_dict)
-    message = diagnose_dnsmasq(error_code, host_id)
+    message = diagnose_dnsmasq(error_code, host_id, dhcp_dict['network_id'])
     return message
 
 def analyze_dnsmasq(dhcp_dict):
@@ -33,12 +33,12 @@ def analyze_dnsmasq(dhcp_dict):
 
     return 0
 
-def diagnose_dnsmasq(error_code, host_id):
+def diagnose_dnsmasq(error_code, host_id, network_id):
 
     if error_code == 0:
-        message = "CODE 0: Successfully identified dnsmasq process for host %s and found MAC and IP for source VM of interest in dnsmasq" % (host_id)
+        message = "CODE 0: Successfully identified dnsmasq process for network %s on host %s and found MAC and IP for source VM of interest in dnsmasq" % (network_id, host_id)
     elif error_code == 1:
-        message = "CODE 1: No dnsmasq process exists for host %s" % (host_id)
+        message = "CODE 1: No dnsmasq process exists for network %s on host %s" % (network_id, host_id)
     else:
-        message = "CODE 2: Successfully identified dnsmasq process for host %s, but not for source VM of interest" % (host_id)
+        message = "CODE 2: Successfully identified dnsmasq process for network %s on host %s, but not for source VM of interest" % (network_id, host_id)
     return message
