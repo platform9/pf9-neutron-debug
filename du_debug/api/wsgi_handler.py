bdb73760edf3e8190c7b513a01615b907fd1b061
Pranav <pranav@platform9.com>
2019-07-23 13:21:36 -0700

    Initial Flask Commit

---
+++ du_debug/api/wsgi_handler.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append("../")
+
+from flask import Flask, request, Response, jsonify
+from oslo_config import cfg
+from oslo_log import log as logging
+import importlib
+import init_neutron_client
+import dhcp_dynamic_info
+import dhcp_static_info
+import du_debug
+import icmp_dynamic_info
+import log_data
+import time
+
+APP_NAME = "confmgr"
+app = Flask(__name__)
+app.debug = True
+
+
+CONF = cfg.CONF
+
+neutron = init_neutron_client.make_neutron_object()
+
+
+@enforce(required=['admin'])
+@app.route('/v1/single/<string:vm_name>', methods=['GET'])
+def single_vm_checker(vm_name):
+
+
+
+@enforce(required=['admin'])
+@app.route('/v1/pair/<string:source_vm>/<string:dest_vm>', methods=['GET'])
+def paired_vms_checker(source_vm, dest_vm):
+
+    icmp_info = icmp_dynamic_info.ICMPInfo(source_vm, dest_vm, neutron)
+    source_icmp_dict = icmp_info.get_source_icmp_dict()
+    dest_icmp_dict = icmp_info.get_dest_icmp_dict()
+    inject_icmp_dict = icmp_info.get_inject_icmp_dict()
+
+    print "SOURCE ICMP DICT"
+    print source_icmp_dict
+    print "DESTINATION ICMP DICT"
+    print dest_icmp_dict
+    print "INJECT SOURCE ICMP DICT"
+    print inject_icmp_dict
+
+    du_debug.listen_on_host(source_icmp_dict)
+    du_debug.listen_on_host(dest_icmp_dict)
+    time.sleep(3)
+    du_debug.source_inject(inject_icmp_dict)
+    time.sleep(3)
+    du_debug.retrieve_listener_data(source_icmp_dict)
+    du_debug.retrieve_listener_data(dest_icmp_dict)
